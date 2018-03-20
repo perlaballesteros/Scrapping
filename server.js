8b040f6f0ceb23019d7fd7e42d242692e725e8d8
Perla Ballesteros <perlaballesteros@email.arizona.edu>
2018-03-20 15:20:33 -0700

    heroku not working

--- server.js
+++ server.js
@@ -30,12 +30,18 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: 'layout' }));
 app.set("view engine", "handlebars");
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/week18ScrappingHw";
-
-// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+var MONGODB_URI = "mongodb://localhost/week18ScrappingHw";
+if(process.env.MONGODB_URI){
+  mongoose.Promise = Promise;
+  mongoose.connect(process.env.MONGODB_URI);
+}
+else{
+  // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect(MONGODB_URI );
+mongoose.connect(MONGODB_URI);
+}
+
 
 //home route
 app.get("/",function(req,res){
